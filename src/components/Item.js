efff059b9205fa820a7412b15fb4e105ed3cb91c
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-08 14:20:03 +0300

    Add to cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,17 @@ import React from "react";
 
 function Item({ name, category }) {
   return (
-    <li className="">
+    <li className="in-cart">
+       
+       {/*If the classname is filled ie<li className="in-cart">
+                      then
+          {/* the item is in the cart }
+          </li>
+
+          If the classname is empty ie <li className=""> 
+                      then 
+          {/* the item is NOT in the cart */}
+
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
