7f6ee284ec94b6b0d829810428bb6eff079dd00a
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-08 14:59:22 +0300

    Error

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,17 +2,8 @@ import React from "react";
 
 function Item({ name, category }) {
   return (
-    <li className="in-cart">
-       
-       {/*If the classname is filled ie<li className="in-cart">
-                      then
-          {/* the item is in the cart }
-          </li>
-
-          If the classname is empty ie <li className=""> 
-                      then 
-          {/* the item is NOT in the cart */}
-
+    <li className= "in-cart">
+      
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
